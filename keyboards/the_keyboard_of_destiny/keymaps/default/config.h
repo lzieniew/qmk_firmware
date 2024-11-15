1805db97747923293349c56dd0e4c15142f2a4e2
lzieniew <lukzieniewicz@gmail.com>
2024-11-15 14:14:09 +0100

    more changes for this keyboard

--- keyboards/the_keyboard_of_destiny/keymaps/default/config.h
+++ keyboards/the_keyboard_of_destiny/keymaps/default/config.h
@@ -1,7 +1,15 @@
 #define TAPPING_TERM 200
+#define PERMISSIVE_HOLD
+#define TAPPING_FORCE_HOLD
+#define RETRO_TAPPING
 
 #define MOUSEKEY_DELAY 0
 #define MOUSEKEY_INTERVAL 16
 #define MOUSEKEY_MOVE_DELTA 8
 #define MOUSEKEY_MAX_SPEED 3
 #define MOUSEKEY_TIME_TO_MAX 60
+
+#define COMBO_TERM 50             // How long for the combo to be pressed (in ms)
+#define COMBO_MUST_HOLD_PER_COMBO // Makes the combo only trigger when held
+#define COMBO_HOLD_TERM 150       // How long to hold for activation (in ms)
+
